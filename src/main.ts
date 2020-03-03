2358476505da67859b32a04401e5a217b0c9f2ce
datlp <1653120@student.hcmus.edu.vn>
2020-03-03 18:59:16 +0700

    fix

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(5000);
+
+  await app.listen(process.env.PORT || '5000');
 }
 bootstrap();
